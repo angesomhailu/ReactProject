d208e1cd41fbd0c098251a6350b72dfd3e023fc1
angesom hailu <angesomhailu1414@gmail.com>
2025-03-22 12:53:22 +0300

    first commit

---
+++ src/component/class.js
@@ -0,0 +1,9 @@
+import react ,{Component} from 'react';
+class Ccomponent extends Component{
+    render(){
+        return (
+            <h1>class component</h1>
+        );
+    }
+}
+export default Ccomponent;
---
+++ src/component/function.js
@@ -0,0 +1,9 @@
+
+function Fcomponent(){
+    return (
+        <div>
+            <h1>the function component page</h1>
+        </div>
+    );
+}
+export default Fcomponent;
